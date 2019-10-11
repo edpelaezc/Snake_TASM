99477a3d5c5c2cab920ce90c8619c20db8281c50
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2019-10-11 09:33:42 -0600

    Agregando validaciones.
    Falta agregar manzanas y crecimiento de la serpiente.

--- proyecto.asm
+++ proyecto.asm
@@ -3,17 +3,17 @@
     bienvenida db 'PROYECTO MICROPROGRAMACION: Snake $'
     instrucciones db 'USE LAS TECLAS: A, S, D W $'
     dimensiones db 'LAS DIMENSIONES INGRESADAS SON INCORRECTAS. $'
+    cerrar db 'HA CERRADO EL JUEGO.$'
+    salida db 'PERDISTE$'
     cadena1 db  "Ingrese X: $";ingrese coordenada x
     cadena2 db  "Ingrese Y: $";ingrese coordenada y
     pared db 219
-    cabeza db '>'      
+    cabeza db '>'  
+    cuerpo db 'O'   
     checkx db ? 
     checky db ? 
     posx db ? 
     posy db ? 
-    msg db 'INVALIDO.$'
-    msgx db 0h 
-    msgy db 0h     
     u db ? ; unidades por si ingresa numero de dos digitos 
     d db ? ; decenas por si ingresa numero de dos digitos     
     x db ? ; pared horizontal del juego 
@@ -84,13 +84,11 @@ programa:
     cmp checkx, 4 
     jz Paso 
 
-    ;MOSTRAR EL TABLERO DE JUEGO 
-    ;clearing the screen 
-    mov ax, 0003H 
+    ;MOSTRAR EL TABLERO DE JUEGO     
+    mov ax, 0003H ;clearing the screen 
     int 10H     
-
-    ;mostrar primera fila 
-    mov cl, x
+    
+    mov cl, x ;mostrar primera fila 
     call FILA 
     xor cl, cl    
     xor dx, dx ; limpinado registros 
@@ -138,60 +136,86 @@ programa:
 ciclo:     
     call LeerKbd
 
-    jmp ciclo 
-
-    jmp FIN 
+    jmp ciclo     
 
 Paso: 
     jmp DimensionesIncorrectas
 
-LeerKbd proc 
+FILA:     
+    mov ah, 02h         
+    mov dh, i 
+    mov dl, j     
+    int 10h ; interrupcion para colocar el cursor 
+    mov dl, pared ; imprimir caracter de la pared
+    mov ah, 02h 
+    int 21h     
+    inc j
+    loop FILA
+    ret 
+
+COLUMNA: 
+    mov ah, 02h         
+    mov dh, i 
+    mov dl, j     
+    int 10h ; interrupcion para colocar el cursor 
+    mov dl, pared ; imprimir caracter de la pared 
+    mov ah, 02h 
+    int 21h     
+    inc i
+    loop COLUMNA  
+    ret 
+
+LeerKbd proc ;LEER ENTRADA DEL TECLADO PARA MOVIMIENTO DE LA SERPIENTE 
     mov ah, 00h
     int 16h
 
     cmp al, 'w'
     jne izquierda    
-    cmp al, 's'
+    cmp cabeza, 'v'
     je retrocedio
-    cmp al, 'x' 
-    je salir
     mov cabeza, '^'
     call moverArriba
+    call validarArr 
     ret
 izquierda: 
     cmp al, 'a'
     jne derecha
-    cmp al, 'd'
+    cmp cabeza, '>'
     je retrocedio 
-    cmp al, 'x'
-    je salir 
     mov cabeza, '<'
+    call moverIzquierda
+    call validarIzq 
     ret
 derecha: 
     cmp al, 'd'
     jne abajo 
-    cmp al, 'a'
+    cmp cabeza, '<'
     je retrocedio 
-    cmp al, 'x'
-    je salir 
-    mov cabeza, '>'    
+    mov cabeza, '>' 
+    call moverDerecha   
+    call validarDer
     ret 
 abajo:
     cmp al, 's'
     jne retrocedio 
-    cmp al, 'w'
+    cmp cabeza, '^'
     je retrocedio 
-    cmp al, 'x' 
-    je salir 
     mov cabeza, 'v'
     call moverAbajo
+    call validarAb 
     ret         
-retrocedio: 
-    ;cmp al, 'x'
-    ;je salir 
+retrocedio:  
+    cmp al, 'x' 
+    je salir
     ret 
 salir: 
-    jmp FIN 
+    mov ax, 0003H ;clearing the screen 
+    int 10H    
+    lea dx, cerrar 
+    mov ah, 09h 
+    int 21h ; mostrar mensaje
+    mov ah, 4CH; terminar juego  
+    int 21H
     ret
 LeerKbd endp 
 
@@ -201,73 +225,100 @@ moverArriba:
     mov dh, posy
     mov dl, posx 
     int 10h ; poner cursor donde irá caracter 
-
     mov ah, 02h 
     mov dl, 32
     int 21h ; escribir espacio en donde estaba el carcter 
-
-    sub posy, 1h ; subir la serpiente           
-
+    sub posy, 1h ; subir la serpiente   
+    ; comparar con checky para ver si está en el borde         
     mov ah, 02h 
     mov dh, posy
     mov dl, posx
     int 10h ; poner cursor donde irá caracter 
-
     mov dl, cabeza
     mov ah, 02h 
     int 21h 
     ret 
-
 moverAbajo: 
     mov ah, 02h 
     mov dh, posy
     mov dl, posx 
     int 10h ; poner cursor donde irá caracter 
-
     mov ah, 02h 
     mov dl, 32
     int 21h ; escribir espacio donde estaba el caracter 
-
     sub posy, -1h ; subir la serpiente           
-
     mov ah, 02h 
     mov dh, posy
     mov dl, posx 
     int 10h ; poner cursor donde irá caracter 
-
     mov dl, cabeza
     mov ah, 02h 
     int 21h 
     ret
-Mover endp 
-
-FILA:     
-    mov ah, 02h     
-    ;mov bh, 0h
-    mov dh, i 
-    mov dl, j     
-    int 10h ; interrupcion para colocar el cursor 
-
-    mov dl, pared
+moverDerecha: 
     mov ah, 02h 
-    int 21h     
-    inc j
-    loop FILA
+    mov dh, posy
+    mov dl, posx 
+    int 10h ; poner cursor donde irá caracter 
+    mov ah, 02h 
+    mov dl, 32
+    int 21h ; escribir espacio donde estaba el caracter 
+    sub posx, -1h ; subir la serpiente           
+    mov ah, 02h 
+    mov dh, posy
+    mov dl, posx 
+    int 10h ; poner cursor donde irá caracter           
+    mov dl, cabeza
+    mov ah, 02h 
+    int 21h          
+    ret
+moverIzquierda: 
+    mov ah, 02h 
+    mov dh, posy
+    mov dl, posx 
+    int 10h ; poner cursor donde irá caracter 
+    mov ah, 02h 
+    mov dl, 32
+    int 21h ; escribir espacio donde estaba el caracter 
+    sub posx, 1h ; subir la serpiente           
+    mov ah, 02h 
+    mov dh, posy
+    mov dl, posx 
+    int 10h ; poner cursor donde irá caracter 
+    mov dl, cabeza
+    mov ah, 02h 
+    int 21h 
+    ret    
+Mover endp     
+
+validacion proc 
+validarArr:     
+    cmp posy, 1
+    je terminaJuego 
     ret 
+validarAb: 
+    mov bl, y 
+    cmp posy, bl
+    je terminaJuego
+    ret
+validarIzq: 
+    cmp posx, 1 
+    je terminaJuego 
+    ret 
+validarDer: 
+    mov bl, x 
+    cmp posx, bl 
+    je terminaJuego 
+    ret
+validacion endp
 
-COLUMNA: 
-    mov ah, 02h     
-    ;mov bh, 0h
-    mov dh, i 
-    mov dl, j     
-    int 10h ; interrupcion para colocar el cursor 
-
-    mov dl, pared
-    mov ah, 02h 
-    int 21h     
-    inc i
-    loop COLUMNA  
-    ret       
+terminaJuego: 
+    mov ax, 0003H ;clearing the screen 
+    int 10H     
+    mov ah, 09h 
+    lea dx, salida
+    int 21h 
+    jmp FIN 
     
 SALTOLINEA:
     ; imprimir un salto de linea antes de mostrar un resultado
